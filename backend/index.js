36457260e0fcf2207ec0183054d0abe393fc7ea7
shivam-chaurasiya <sschaurasiya5783@gmail.com>
2023-03-07 11:29:14 +0530

    work on post api

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
 const connectDB = require('./config/db');
 const bodyparser = require('body-parser');
 const Router = require('./routes/route');
+//const { default: App } = require('../frontend/src/App');
 
 
 connectDB();
@@ -12,13 +13,17 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-
+app.use(cors());
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 
 
 app.use('/project',Router);
+
+
+
+
 
 
 const PORT = process.env.PORT || 8080;
