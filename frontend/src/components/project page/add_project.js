105d0dc03e19a3561cdcaadd573330c93c406072
Sushma Rani <rajputsushma5@gmail.com>
2023-03-07 11:48:21 +0530

    changes

--- frontend/src/components/project page/add_project.js
+++ frontend/src/components/project page/add_project.js
@@ -1,16 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 function Add() {
   const navigate = useNavigate();
+  const [data , setData] = useState();
+
+  useEffect(() => {
+    fetch('/ads').then(response => {
+      setData(response.json())
+    })
+  }, [])
+  
   return (
     <div>
       <Header />
 
-      <div className="row form_container" style={{display: "flex"}}>
+      <div className="row form_container">
         <div className="col-md-3 lead " style={{ fontSize: "1.5rem" }}>
           Inzint's project
         </div>
-        <div className="col-md-3 col-md-offset-6 pull-right-12">
+        <div className="col-md-3 col-md-offset-6 pull-right-12" style={{display:'right'}}>
           <button
             type="button"
             className="btn btn-outline-primary"
@@ -22,7 +31,7 @@ function Add() {
           </button>
         </div>
       </div>
-      <div class="col-md-9" style={{width:'100%',margin:'45px'}}>
+      <div class="col-md-9" style={{width:'100%',marginTop:'45px'}}>
   <table class="table table-hover">
     <thead>
       <tr>
