c0ba90295280ea189ad5f25f7e9240aaa01f4e8f
Sushma Rani <rajputsushma5@gmail.com>
2023-03-07 11:48:21 +0530

    changes

--- frontend/src/components/project page/Header.js
+++ frontend/src/components/project page/Header.js
@@ -5,8 +5,8 @@ function Header() {
 	<div className="header border">
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="https://inzint.com
-"><img src="https://inzint.com/wp-content/uploads/2022/09/inzint-logo-dark.png" width="80px"/></a>
+    <a className="navbar-brand" href="https://inzint.com">
+<img src="https://inzint.com/wp-content/uploads/2022/09/inzint-logo-dark.png" width="80px"/></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
