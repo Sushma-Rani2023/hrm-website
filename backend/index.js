c2a009103e1b7b9269513b07d1009f2e154261c4
shivam-chaurasiya <sschaurasiya5783@gmail.com>
2023-03-21 17:59:37 +0530

    passport-saml

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
-
 const connectDB = require('./config/db');
 const bodyparser = require('body-parser');
 const Router = require('./routes/route');
@@ -10,8 +8,9 @@ const Router1 = require('./routes/Clientroutes');
 const Router2 =require('./routes/engineer');
 const Router4 = require('./routes/Taskroutes');
 const RouterT = require('./routes/Team');
-//const { default: App } = require('../frontend/src/App');
 const app = express();
+const passport = require('passport');
+const SamlStrategy = require('passport-saml').Strategy;
 const serverless = require('serverless-http')
 
 connectDB();
@@ -23,6 +22,38 @@ app.use(cors());
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
+
+const config = {
+    saml: {
+      path: '/login/callback',
+      entryPoint: 'https://login.microsoftonline.com/383cc6fe-545e-4fab-8276-0c78ac6258f7/saml2',
+      issuer: 'a7ce44f1-a25e-45d2-83eb-3aec6772f292',
+      cert: '25821fa3-15c8-47c2-83ee-d24d6e311e9c',
+      identifierFormat: null,
+      signatureAlgorithm: 'sha256',
+      acceptedClockSkewMs: 60 * 1000,
+    },
+  };
+
+  passport.use(new SamlStrategy(config.saml, (profile, done) => {
+    console.log('SAML profile:', profile);
+    return done(null, profile);
+  }));
+  
+  app.get('/login', passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }), (req, res) => {
+    console.log('SAML login successful');
+    res.redirect('/');
+  });
+  
+  app.post(config.saml.path, passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }), (req, res) => {
+    console.log('SAML callback successful');
+    res.redirect('/');
+  });
+  
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
 
 
 app.use('/project',Router);
