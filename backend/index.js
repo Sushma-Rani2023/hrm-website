9665fd7c9369df325bc0cdd55fdbcc5e9d922863
Sushma Rani <rajputsushma5@gmail.com>
2023-06-26 14:42:05 +0530

    chnges intgrating

--- backend/index.js
+++ backend/index.js
@@ -30,12 +30,10 @@ app.use(bodyparser.json());
 app.use(express.static(path.resolve(__dirname, "public")));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env._fronturl);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", process.env._fronturl);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
   next();
 });
 
